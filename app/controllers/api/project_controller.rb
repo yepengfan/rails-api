537f5e906829abcb9eec9a62194f3fc10375da37
Yepeng Fan <yepeng.fan@hotmail.com>
2016-03-30 23:07:26 +1100

    Add routes

--- app/controllers/api/project_controller.rb
+++ app/controllers/api/project_controller.rb
@@ -1,6 +1,21 @@
 class Api::ProjectController < ApplicationController
+  protect_from_forgery
+  skip_before_action :verify_authenticity_token, if: :json_request?
+
   def index
     @projects = Project.all
     render json: @projects
+  end
+
+  def import_json
+    puts params[:project]
+    Project.import params[:project]
+    render :nothing => true, :status => :no_content
+  end
+
+  protected
+
+  def json_request?
+    request.format.json?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
   get 'project/index'
+  post 'project/import_json'
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
